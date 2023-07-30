878cf5e054635ab68bf14360bc9fa568ae55cf31
Mason Boyles <mboyles@unc.edu>
2023-07-30 04:41:47 -0400

    Nutrition2

--- src/app/Components/nutrition-facts/nutrition-facts.component.ts
+++ src/app/Components/nutrition-facts/nutrition-facts.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { MealserviceService } from 'src/app/Services/mealservice.service';
 import { ChosenFood } from 'src/assets/chosen-food';
 import { FoodObject } from 'src/assets/food-object';
+import { FoodTotals } from 'src/assets/food-totals';
 
 @Component({
   selector: 'app-nutrition-facts',
@@ -10,6 +11,34 @@ import { FoodObject } from 'src/assets/food-object';
 })
 export class NutritionFactsComponent {
   @Input() selectedFoods!: ChosenFood[];
+
+  cummulitiveTotals: FoodTotals = {
+    "Amount Per Serving": 0,
+    "Calories": 0,
+    "Calories from Fat": 0,
+    "Total Fat": 0,
+    "Saturated Fat": 0,
+    "Trans Fat": 0,
+    "Cholesterol": 0,
+    "Sodium": 0,
+    "Total Carbohydrate": 0,
+    "Dietary Fiber": 0,
+    "Sugars": 0,
+    "Protein": 0
+  };
+
+  public updateTotals(){
+    let last = Object(this.selectedFoods[-1].food)
+    let keys = Object.keys(last.food)
+    for (const key of keys) {
+      const regex = /\d+/g; // Regular expression to match one or more digits
+      const matches = last[key].match(regex);
+      Object(this.cummulitiveTotals)[key] += Number(matches)
+    }
+  }
+
+
+
   constructor(){}
   
 }
---
+++ src/assets/food-totals.ts
@@ -0,0 +1,14 @@
+export interface FoodTotals {
+    "Amount Per Serving": number,
+    "Calories": number,
+    "Calories from Fat": number,
+    "Total Fat": number,
+    "Saturated Fat": number,
+    "Trans Fat": number,
+    "Cholesterol": number,
+    "Sodium": number,
+    "Total Carbohydrate": number,
+    "Dietary Fiber": number,
+    "Sugars": number,
+    "Protein": number
+}
