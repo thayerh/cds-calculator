9a08ea6eba2e044dfd541158d10a14a2521233ed
Mason Boyles <mboyles@unc.edu>
2023-07-29 22:45:29 -0400

    partially owrking

---
+++ src/app/Services/mealservice.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import menuList from '../../../scrape/results2.json'
+import { FoodItem } from 'src/assets/food-item';
+import { DaysMenu } from 'src/assets/days-menu';
+import { Menu } from 'src/assets/menu';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MealserviceService {
+
+  menu= menuList;
+
+  constructor() { }
+}
